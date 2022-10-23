086c4717d4fa0fb1bd5a54b73dd88d6f6a739a64
iarturkea <79039593+iarturkea@users.noreply.github.com>
2022-10-23 13:53:07 +0700

    Add files via upload

---
+++ Lesson2p1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Input num");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(num / 10);
+
---
+++ Lesson2p3/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Input day number");
+int dayNum = int.Parse(Console.ReadLine());
+if(dayNum == 6 | dayNum == 7) Console.WriteLine("Yes");
+else Console.WriteLine("No");
