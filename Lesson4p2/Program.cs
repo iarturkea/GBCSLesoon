c00b1c84012b14e73d5217692858f20473d7cfa1
iarturkea <79039593+iarturkea@users.noreply.github.com>
2022-10-30 12:34:48 +0700

    Add files via upload

---
+++ Lesson4p2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input num");
+int num = int.Parse(Console.ReadLine());
+int res = 0;
+while(true)
+{
+ res = res + num % 10;
+ num = num / 10;
+
+ if (num == 0)break;
+}
+
+ Console.WriteLine("Summ = " + res);
---
+++ Lesson4p3/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int[] numArr = new int[8];
+Console.WriteLine("Input Array");
+for (int i = 0; i < numArr.Length; i++)
+{
+    Console.WriteLine("Input num " + (i+1));
+    numArr[i] = int.Parse(Console.ReadLine());
+    
+}
+Console.Write("[");
+
+for (int i = 0; i < 4; i++)
+{
+    Console.Write(numArr[i] + ", ");
+}
+Console.WriteLine(numArr[4] + "]");
+Console.WriteLine("["+ numArr[5] + ", " + numArr[6] + ", " + numArr[7] + "]" );
+
