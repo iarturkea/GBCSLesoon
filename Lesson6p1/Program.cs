8ef269e1672a3027cbb1430ec4cb89dcb0dc629c
iarturkea <79039593+iarturkea@users.noreply.github.com>
2022-11-20 13:31:33 +0700

    Add files via upload

---
+++ Lesson6p1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input num amount");
+int am = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+for (int i = 0; i < am; i++)
+{
+    Console.WriteLine("Input num " + (i + 1));
+    int num = int.Parse(Console.ReadLine());
+
+    if (num > 0) count++;
+}
+
+
+ Console.WriteLine("Amount numbers above zero = " + count);
---
+++ Lesson6p2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("input line 1 parm: A1x + B1y + C1 = 0" );
+Console.WriteLine("input A1");
+double a1 = double.Parse(Console.ReadLine());
+Console.WriteLine("input B1");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("input C1");
+double c1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("input line 2 parm: A2x + B2y + C2 = 0");
+Console.WriteLine("input A2");
+double a2 = double.Parse(Console.ReadLine());
+Console.WriteLine("input B2");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("input C2");
+double c2 = double.Parse(Console.ReadLine());
+
+if(a1 * b2 - a2 * b1 == 0) Console.WriteLine("Lines are equal or parallel");
+
+else{
+
+    double x = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1);
+    double y = (a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1);
+
+    Console.WriteLine("lines intersection point: [" + x + ", " + y + "]");
+
+}
