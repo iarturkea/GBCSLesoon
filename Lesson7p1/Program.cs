a2ff6fced9766c95d0940c8e85ce8c5a26974126
iarturkea <79039593+iarturkea@users.noreply.github.com>
2022-11-27 16:17:17 +0700

    Add files via upload

---
+++ Lesson7p1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+void FillArray (double[,] array){
+
+
+    int rows = array.GetUpperBound(0) + 1;
+    int colums = array.Length / rows;
+
+    for (int i = 0; i < rows; i++)
+    {
+        
+        for (int j = 0; j < colums; j++)
+
+        {
+            array[i, j] = new Random().Next(-100,100)/10.0;
+            
+        }
+        
+    }
+
+
+}
+
+Console.WriteLine("Input array length");
+Console.WriteLine("Input m");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Input n");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+FillArray(array);
+
+
+
---
+++ Lesson7p2/Program.cs
@@ -0,0 +1,43 @@
+﻿
+void FillArray (double[,] array){
+
+
+    int rows = array.GetUpperBound(0) + 1;
+    int colums = array.Length / rows;
+
+    for (int i = 0; i < rows; i++)
+    {
+        
+        for (int j = 0; j < colums; j++)
+
+        {
+            array[i, j] = new Random().Next(-100,100)/10.0;
+            
+        }
+        
+    }
+
+
+}
+
+Console.WriteLine("Input array length");
+Console.WriteLine("Input m");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Input n");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+FillArray(array);
+
+Console.WriteLine("Input element position");
+Console.WriteLine("Input row");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Input colum");
+int b = int.Parse(Console.ReadLine());
+
+int rows = array.GetUpperBound(0) + 1;
+int colums = array.Length / rows;
+
+if (a < rows && b < colums)Console.WriteLine("Element: " + array[a, b]);
+else Console.WriteLine("no such element");
