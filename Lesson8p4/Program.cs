234bbceddf03ce5ae57de1119170ec8730e69ad9
iarturkea <79039593+iarturkea@users.noreply.github.com>
2022-11-27 18:46:37 +0700

    Add files via upload

---
+++ Lesson8p4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int[,,] array = new int[2,2,2] { { { 22, 33}, { 44, 52 } } , { { 71, 82}, { 10, 11 } } };
+
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        for (int k = 0; k < 2; k++)
+        {
+            Console.Write(array[i,k,j] + "(" + i + "," + k + "," + j + ")");
+        }
+        Console.WriteLine();
+    }
+}
+
